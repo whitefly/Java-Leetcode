4268c478d0249241bbc71dd98087d02c4bfb1f3f
zhouang <316447676@qq.com>
2018-12-12 01:10:42 +0800

    1.no_43 multiply-strings(大数乘法)

--- src/双指针/Q15_3sum.java
+++ src/双指针/Q15_3sum.java
@@ -18,7 +18,6 @@ public class Q15_3sum {
             while (j < k) {
                 while (j < k && j != i + 1 && nums[j - 1] == nums[j]) j++;  //只使用首次出现的数
                 while (j < k && k != nums.length - 1 && nums[k] == nums[k + 1]) k--;
-
                 if (j == k) break;
 
                 int num2 = nums[j];
@@ -38,10 +37,46 @@ public class Q15_3sum {
         return result;
     }
 
+    public List<List<Integer>> threeSum2(int[] nums) {
+        /**
+         * 先比较,后跳过重复,可以减少少许代码量.
+         * 但是逻辑性没有第一次强
+         */
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        int size = nums.length - 1;
+        //固定指针
+        for (int i = 0; i <= size; i++) {
+            if (i != 0 && nums[i - 1] == nums[i]) continue;
+            int num1 = nums[i];
+
+            //双指针寻找余下2个数
+            int j = i + 1, k = size;
+            while (j < k) {
+                int num2 = nums[j];
+                int num3 = nums[k];
+                int sum = num1 + num2 + num3;
+                if (sum == 0) {
+                    result.add(Arrays.asList(num1, num2, num3));
+                    j++;
+                    k--;
+                    while (j < k && nums[j - 1] == nums[j]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (sum < 0) {
+                    j++;    //若出现重复,使用顶层的while来去重
+                } else {
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        int nums[] = {-2, 0, 0, 2, 2};
+        int nums[] = {
+                -4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
         Q15_3sum s = new Q15_3sum();
-        List<List<Integer>> lists = s.threeSum(nums);
+        List<List<Integer>> lists = s.threeSum2(nums);
         System.out.println(lists);
 
     }
---
+++ src/字符串/Q43_multiply_strings.java
@@ -0,0 +1,54 @@
+package 字符串;
+
+import java.util.Arrays;
+
+public class Q43_multiply_strings {
+    public String multiply(String num1, String num2) {
+        /**
+         * 字符串转数字,
+         *思入: 数组每个元素先存2个数字成绩之和,然后进行规整化(即转为10进制),然后反转字符串
+         *
+         */
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        // 加速
+        if (num1.equals("1")) return num2;
+        if (num2.equals("1")) return num1;
+
+        //估计大致位数
+        int num1_size = num1.length(), num2_size = num2.length();
+        int[] result = new int[num1_size * num2_size + 2];  //---> 从小到大
+        Arrays.fill(result, 0);
+        // 无脑相加到特定位置
+        int n1, n2;
+        for (int i = num1_size - 1; i >= 0; i--) {
+            n1 = num1.charAt(i) - '0';
+            for (int j = num2_size - 1; j >= 0; j--) {
+                //加入特定的位置
+                n2 = num2.charAt(j) - '0';
+                int position = (num1_size - 1 - i) + (num2_size - 1 - j);
+                result[position] += n1 * n2;
+            }
+        }
+        // 规整化
+        int last_index = num1_size + num2_size - 1;
+        int carry = 0, temp;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i <= last_index; i++) {
+            temp = result[i] + carry;
+            carry = temp / 10;
+            res.append(temp % 10);
+        }
+        //反转字符串,去掉0
+        String s = res.reverse().toString();
+        System.out.println(s);
+        if (s.startsWith("0"))
+            return s.substring(1);
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Q43_multiply_strings s = new Q43_multiply_strings();
+        String l = s.multiply("99", "99");
+        System.out.println(l);
+    }
+}
