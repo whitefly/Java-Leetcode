f1c132a0d472262a18b1c7a8b3626e9bfaddc146
zhouang <316447676@qq.com>
2019-02-12 18:59:55 +0800

    60 排列组合

--- src/排列组合/Q60_permutation_sequence.java
+++ src/排列组合/Q60_permutation_sequence.java
@@ -1,6 +1,7 @@
 package 排列组合;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Q60_permutation_sequence {
@@ -44,9 +45,34 @@ public class Q60_permutation_sequence {
         }
     }
 
+    public String getPermutation2(int n, int k) {
+        /**
+         * 思入: 通过k,来直接确定每次的数字,然后计算新k
+         */
+        LinkedList<Integer> nums = new LinkedList<>();
+        for (int i = 1; i <= n; i++) nums.add(i);
+        StringBuilder result = new StringBuilder();
+
+        k--;
+        int group_size = 1;  //group_size为每次的阶乘,分别为(n-1)!,(n-2)!...直到为1
+        for (int i = 1; i < n; i++) group_size *= i;
+
+        while (nums.size() > 1) {
+            //添加数字
+            int group_id = k / group_size;
+            result.append(nums.get(group_id));
+            //减去nums中的数字,并计算新下标
+            k %= group_size;
+            nums.remove(group_id);
+            group_size /= nums.size();
+        }
+        result.append(nums.get(0));
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         Q60_permutation_sequence s = new Q60_permutation_sequence();
-        String permutation = s.getPermutation(4, 9);
+        String permutation = s.getPermutation2(4, 9);
         System.out.println(permutation);
     }
 }
