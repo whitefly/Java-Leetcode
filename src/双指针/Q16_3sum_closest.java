56f59727ccf42d744c2624dfac67cbc3323aff82
zhouang <316447676@qq.com>
2019-03-04 00:31:26 +0800

    16 75 双指针+快排

--- src/双指针/Q16_3sum_closest.java
+++ src/双指针/Q16_3sum_closest.java
@@ -1,6 +1,7 @@
 package 双指针;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class Q16_3sum_closest {
     public int threeSumClosest(int[] nums, int target) {
@@ -28,11 +29,40 @@ public class Q16_3sum_closest {
         return target - min_gap;
     }
 
+    public int threeSumClosest2(int[] nums, int target) {
+        /**s
+         * 思入: 设置一个变量来保存差值和最近的数
+         */
+        int gap = Integer.MAX_VALUE;
+        int result = 0;
+        int size = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < size; i++) {
+            //使用双指针
+            int n1 = nums[i];
+            int L = i + 1;
+            int R = size - 1;
+            while (L < R) {
+                int n2 = nums[L], n3 = nums[R];
+                int sum = n1 + n2 + n3;
+                if (sum == target) return target;
+                int temp_gap = Math.abs(target - sum);
+                if (temp_gap < gap) {
+                    gap = temp_gap;
+                    result = sum;
+                }
+                if (target < sum) R--;
+                else L++;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] nums = {-1, 2, 1, -4};
         int tagrget = 1;
         Q16_3sum_closest s = new Q16_3sum_closest();
-        int i = s.threeSumClosest(nums, tagrget);
+        int i = s.threeSumClosest2(nums, tagrget);
         System.out.println(i);
 
     }
---
+++ src/双指针/Q75_sort_colors.java
@@ -0,0 +1,37 @@
+package 双指针;
+
+import java.util.Arrays;
+
+public class Q75_sort_colors {
+    public void sortColors(int[] nums) {
+        /**
+         * 思入: 使用双指针, 用L指针保存未来存放0的位置,R指针保存未来存储2的位置,只要保证0,2归位,1不用管
+         * 实现:
+         * 碰到[current]=0, 则交换L,L++,current++(因为current比L快,都会验证过,不需要在重复验证,换过来一定是1),开始下一轮
+         * 碰到[current]=1,则忽略,current++,
+         * 碰到[current]=2,则交换R,R--,开始下一轮
+         */
+
+        int L = 0, index = 0, R = nums.length - 1;
+        while (index < R) {
+            int num = nums[index];
+            if (num == 0) {
+                swap(nums, L++, index++);
+            } else if (num == 1) index++;
+            else swap(nums, R--, index);
+        }
+    }
+
+    public void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+
+    public static void main(String[] args) {
+        Q75_sort_colors s = new Q75_sort_colors();
+        int[] nums = {1, 1, 1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 0, 2, 1, 1, 1, 2, 1, 0, 2, 2, 1, 1, 1, 1};
+        s.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
