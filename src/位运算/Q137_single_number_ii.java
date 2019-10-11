4285848f59cfe9e0c09c62f3c67696c06a46df5b
zhouang <316447676@qq.com>
2019-10-11 22:45:19 +0800

    Q137(状态机)

--- src/位运算/Q137_single_number_ii.java
+++ src/位运算/Q137_single_number_ii.java
@@ -35,12 +35,32 @@ public class Q137_single_number_ii {
         return sizeBits[n];
     }
 
+    public int singleNumber3(int[] nums) {
+        /*
+        思入3: 每一位用一个int来表示状态,重复出现几次就有多少种状态.用转移状态矩阵来解题.这应该是最好理解的做法
+         */
+        int[][] matrix = {{0, 1, 2}, {1, 2, 0}};
+        int[] status = new int[32];
+        for (int num : nums) {
+            for (int i = 0, bit = num & 1; i < 32 && num != 0; num >>= 1, bit = num & 1, i++) {
+                status[i] = matrix[bit][status[i]];
+            }
+        }
+        //还原
+        int result = 0;
+        for (int i = 0; i < 32; i++) result += status[i] << i;
+        return result;
+    }
+
     public static void main(String[] args) {
 //        int[] nums = {2, 2, 3, 2, 3, 3, 99};
-        int[] nums = {3, 3, 3, 1};
+//        int[] nums = {3, 3, 3, 1};
+        int[] nums = {1, 1, 1, 2};
         Q137_single_number_ii s = new Q137_single_number_ii();
         int i = s.singleNumber2(nums);
         System.out.println(i);
+
+        System.out.println(s.singleNumber3(nums));
     }
 
 }
